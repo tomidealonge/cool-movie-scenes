e19b346a36b6cc2e497c701b8b495293922b4c65
Tola <akintomide.alonge@gmail.com>
2021-05-29 15:07:44 +0100

    added some gifs and made fixed bubbles mobile

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
-// const target = document.querySelectorAll('.split-test')
-// const results = Splitting({ target: target, by: 'lines' });
-// const lines = results[0].lines
-// console.log(results)
-// const newlines = lines.map((el) => {
-//     return el.map((el) => {
-//       return el.textContent
-//     })
-// })
-// const newest = newlines.map((line) => {
-//   return line.join(' ')
-// })
+const target = document.querySelectorAll('.split-test')
+const results = Splitting({ target: target, by: 'lines' });
+const lines = results[0].lines
+console.log(results)
+const newlines = lines.map((el) => {
+    return el.map((el) => {
+        return el.textContent
+    })
+})
+const newest = newlines.map((line) => {
+    return line.join(' ')
+})
 
-// function createObserver() {
-//   let observer
+function createObserver() {
+    let observer
 
-//   let options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: buildThresholdList(),
-//   }
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: buildThresholdList(),
+    }
 
-//   observer = new IntersectionObserver(handleIntersect, options)
-//   observer.observe(boxElement)
-// }
+    observer = new IntersectionObserver(handleIntersect, options)
+    observer.observe(boxElement)
+}
 
-// console.log(newest)
-// console.log(newlines)
-// gsap.from(results[0].lines, {
-//   duration: 5.5,
-//   opacity: 0,
-//   ease: 'back',
-//   rotationX: -120,
-// })
-
+console.log(newest)
+console.log(newlines)
+gsap.from(results[0].lines, {
+    duration: 5.5,
+    opacity: 0,
+    ease: 'back',
+    rotationX: -120,
+})
